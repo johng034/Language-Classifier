a1be4ab61c77f36af161ee32c7464669106ec290
JohnG2055 <51770537+JohnG2055@users.noreply.github.com>
2021-10-10 22:29:29 -0700

    improved the web app and cleaned up some code in the jupyter notebooks

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import streamlit as st
 import time
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import pyautogui
+import re
+# import numpy as np
 
 # DOCUMENTATION
 # Streamlit
@@ -11,14 +13,24 @@ import matplotlib.pyplot as plt
 
 
 # save the model to disk
-filename = 'saved-items/finalized_model.sav'
-file = 'saved-items/language.sav'
-n = 'naive.sav'
+model_file = './saved-items/naive_bayes.sav'
+language_file = './saved-items/languages.sav'
+# naive = 'naive.sav'
  
 # load the model from disk
-model = pickle.load(open(filename, 'rb'))
-languages = pickle.load(open(file, 'rb'))
-naive = pickle.load(open(n, 'rb'))
+model = pickle.load(open(model_file, 'rb'))
+languages = pickle.load(open(language_file, 'rb'))
+# naive = pickle.load(open(n, 'rb'))
+
+# Preprocess function
+def preprocess(text):
+    text = re.sub("[\[\[\]\]?—\"\"«»]", "", text)  # Remove special characters
+    text = text.replace('\u200b', '')
+    text = re.sub("-", " ", text)  # Replace '-' with a space
+    text = " ".join(text.split())  # Remove any extra spaces
+    text = text.lower()
+    
+    return text
 
 # Streamlit app
 st.title("Language Classifier")
@@ -29,31 +41,54 @@ st.markdown("Hello and welcome! For this project, I trained a Naive Bayes Classi
 
 # def run_algo():
 text = st.text_input("Please type a sentence: ")
+text = preprocess(text)
 
 if bool(text) or st.button("Classify"):
-    st.header("Prediction")
-    prediction = str(model.predict([text])[0])
-    st.subheader("The classifier predicts this language is: ")
-    time.sleep(0.2)
-    st.subheader(prediction)
+    if not bool(text):
+        st.subheader("Please type a sentence.")
+        if st.button('Reset'):
+            pyautogui.hotkey('command','r')
+        else:
+            time.sleep(5)
+            pyautogui.hotkey('command','r')
+
+    else:
+        st.header("Prediction")
+        prediction = str(model.predict([text])[0])
+        st.subheader("The classifier predicts this language is: ")
+        time.sleep(0.2)
+        probabilities = model.predict_proba([text])
+        probs = probabilities[0]
+
+        if all([x == probs[0] for x in probs]):
+            st.subheader('Unsure')
+            st.markdown("The model is not sure which language this is. Any prediction would be a random guess. \
+            Please try again with more text.")
+            if st.button('Reset'):
+                pyautogui.hotkey('command','r')
+        else:
+            st.subheader(prediction)
 
 
-    # Bar graph
-    probabilities = model.predict_proba([text])
-    # st.text(probabilities)
-    # st.text(languages)
+            # Bar graph
+            # probabilities = model.predict_proba([text])
+            # st.text(probabilities)
+            # st.text(languages)
 
-    df = pd.DataFrame(probabilities, columns=languages)
-    # st.dataframe(df)
+            df = pd.DataFrame(probabilities, columns=languages)
+            # st.dataframe(df)
 
-    fig, ax = plt.subplots()
-    ax.bar(x=languages, height=probabilities.reshape(10,), width=0.8)
-    st.pyplot(fig)
+            fig, ax = plt.subplots()
+            ax.bar(x=languages, height=probabilities.reshape(10,), width=0.8)
+            ax.tick_params(labelrotation=45)
+            st.pyplot(fig)
 
-    st.markdown((naive.coef_.size))
+            # st.markdown((naive.coef_.size))
 
-    # plt.figure(figsize=(12, 8))
-    # plt.bar(x=langs, height=pred_probs.reshape(10,), width=0.8)
-    # plt.show()
+            # plt.figure(figsize=(12, 8))
+            # plt.bar(x=langs, height=pred_probs.reshape(10,), width=0.8)
+            # plt.show()
 
-    # st.bar_chart(df)
+            # st.bar_chart(df)
+            if st.button('Reset'):
+                pyautogui.hotkey('command','r')
